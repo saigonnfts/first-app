3a130dc5f21dab01e874fc1d189ec03771151c10
Sai Gon NFTs <109427820+saigonnfts@users.noreply.github.com>
2022-10-23 00:55:14 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          You Can Learn React
         </a>
         <p>
           My First <code>App</code> in NodeJS.
